aeb455cb0432b64bb71c4841aea6fc9c87f6f40a
Patrick José Pereira <patrickelectric@gmail.com>
2022-01-19 11:09:16 -0300

    settings: Random corrections
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- settings.py
+++ settings.py
@@ -5,17 +5,22 @@ import pathlib
 import pykson
 from pykson import Pykson
 
+
 class BadSettingsFile(ValueError):
     """Settings file is not valid."""
+
 
 class SettingsFromTheFuture(ValueError):
     """Settings file version is from a newer version of the service."""
 
+
 class MigrationFail(RuntimeError):
     """Could not apply migration."""
 
+
 class BadAttributes(BadSettingsFile):
     """Attributes on settings file are not valid."""
+
 
 class BaseSettings(pykson.JsonObject):
     """Base settings class that has version control and struct based serialization/deserialization"""
@@ -49,9 +54,7 @@ class BaseSettings(pykson.JsonObject):
             result = json.load(settings_file)
 
             if "VERSION" not in result.keys():
-                raise BadSettingsFile(
-                    f"Settings file does not appears to contain a valid settings format: {result}"
-                )
+                raise BadSettingsFile(f"Settings file does not appears to contain a valid settings format: {result}")
 
             version = result["VERSION"]
 
@@ -60,7 +63,8 @@ class BaseSettings(pykson.JsonObject):
 
             if version > self.VERSION:
                 raise SettingsFromTheFuture(
-                    f"Settings file comes from a future settings version: {version}, latest supported: {self.VERSION}, tomorrow does not exist"
+                    f"Settings file comes from a future settings version: {version}, "
+                    f"latest supported: {self.VERSION}, tomorrow does not exist"
                 )
 
             if version < self.VERSION:
@@ -68,9 +72,7 @@ class BaseSettings(pykson.JsonObject):
                 version = result["VERSION"]
 
             if version != self.VERSION:
-                raise MigrationFail(
-                    "Migrate chain failed to update to the latest settings version available"
-                )
+                raise MigrationFail("Migrate chain failed to update to the latest settings version available")
 
             # Copy new content to settings class
             new = Pykson().from_json(result, self.__class__)
@@ -87,11 +89,10 @@ class BaseSettings(pykson.JsonObject):
         if not parent_path.exists():
             parent_path.mkdir(parents=True, exist_ok=True)
 
-        with open(file_path, "w") as settings_file:
+        with open(file_path, "w", encoding="utf-8") as settings_file:
             settings_file.write(Pykson().to_json(self))
 
     def reset(self):
-        """Reset internal data to default values
-        """
+        """Reset internal data to default values"""
         new = self.__class__()
         self.__dict__.update(new.__dict__)
